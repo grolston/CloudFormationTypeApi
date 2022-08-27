7d0432e3d21d3d7916c983dbd512c676c984914e
George Rolston <grolston@users.noreply.github.com>
2022-08-26 19:22:54 -0600

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,13 @@
+IPMO AWSPowerShell.NetCore
+$Resources = Get-CFNTypeList -Type Resource -Visibility PUBLIC -Filters_Category AWS_Types | Select TypeName
+$i =1
+mkdir -p ./output
+Foreach ($resource in $Resources){
+  $resourceName = $resource.TypeName
+  $fileName = $resourceName.replace('::', '-')
+  $JsonObj = $(Get-CFNType -TypeName $resourceName -Type Resource).Schema
+  $JsonObj | Out-File ./output/$fileName.json -Encoding utf8
+  Write-Host "completed $i of $($resources.count)…"
+  if($i % 10  -eq 0) {write-host "throttling 5 seconds"; sleep 5}
+  $i++
+}
